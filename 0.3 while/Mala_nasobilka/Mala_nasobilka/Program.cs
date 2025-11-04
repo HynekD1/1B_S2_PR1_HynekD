daaa7779dc90eabb28abdae26123e573812aab66
HynekD1 <dominik.hynek343@gmail.com>
2025-11-04 08:58:57 +0100

    ok

---
+++ 0.3 while/Mala_nasobilka/Mala_nasobilka/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Mala_nasobilka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nasobky = 7;
+            int nasobitel = 0;
+            while (nasobitel <= 10)
+            {
+                Console.WriteLine(nasobky * nasobitel);
+                nasobitel++;
+            }
+        }
+    }
+}
---
+++ 0.3 while/nacti_sudecislo/nacti_sudecislo/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace nacti_sudecislo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cislo = 0;
+
+            do
+            {
+                Console.WriteLine("Zadej mi sudé číslo:");
+                cislo = int.Parse(Console.ReadLine());
+            } while (cislo % 2 == 1);
+            Console.WriteLine($"Sudé zadané číslo je {cislo} a jeho nejbližší vyšší liché je {cislo+1}");
+
+        }
+    }
+}
