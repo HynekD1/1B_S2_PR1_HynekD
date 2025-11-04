df915cec20cd20cd7731a39a976bd754915358bc
HynekD1 <dominik.hynek343@gmail.com>
2025-11-04 09:32:55 +0100

    ok

---
+++ 0.3 while/Soucet_cisel_v_intervalu/Soucet_cisel_v_intervalu/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Soucet_cisel_v_intervalu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadej okraj intervalu");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Zadej okraj intervalu");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            int max = Math.Max(x, y); // Uloží to do max větší z čísel
+            int min = Math.Min(x, y); // Uloží to do min menší z čísel
+
+            int soucet = 0;
+            
+            while(min <= max)
+            {
+                soucet += min;
+                min++;
+            }
+            Console.WriteLine($"Součet čísel v intervalu (včetně okrajů) je {soucet}");
+
+        }
+    }
+}
---
+++ 0.3 while/delitelnost/delitelnost/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace delitelnost
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadej číslo kterým budete chtít dělit");
+            int delitel = Convert.ToInt32(Console.ReadLine());
+            int cislo = 0;
+
+            do
+            {
+                Console.WriteLine($"Zadej číslo dělitelné {delitel}");
+                cislo = Convert.ToInt32(Console.ReadLine());
+
+
+            } while (cislo % delitel != 0 );
+            Console.WriteLine($"{cislo} / {delitel} = {cislo / delitel}");
+        }
+    }
+}
