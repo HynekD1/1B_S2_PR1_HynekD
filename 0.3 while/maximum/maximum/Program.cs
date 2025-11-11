3f0ca7ec952fd6f55ebaa4cfd91aa5df8f90a79a
HynekD1 <dominik.hynek343@gmail.com>
2025-11-11 08:45:01 +0100

    ok

---
+++ 0.3 while/maximum/maximum/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace maximum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //proměnná do které budu ukládat čísla zadané uživatelem
+            int cislo = 0;
+
+            //proměnná pro zapsání nalezeného maxima
+            int max = 0;
+            
+            do
+            {
+                Console.WriteLine("Zadej mi číslo");
+                cislo = Convert.ToInt32(Console.ReadLine());
+                if(cislo > max)
+                {
+                    max = cislo;
+                }
+
+            } while (cislo > 0);
+
+            //Cyklus se opakuje dokud je v proměnné číslo uložená hodnota vyšší než 0
+
+            Console.WriteLine($"Nejvyšší zadané číslo je {max}");
+
+        }
+    }
+}
