26b863cba71c220513a6cbaefdf484a20c4417a5
HynekD1 <dominik.hynek343@gmail.com>
2025-11-18 08:44:09 +0100

    ok

---
+++ 0.3 while/Prumerny_hod_kostkou/Prumerny_hod_kostkou/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Prumerny_hod_kostkou
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int steny_kostky = 0;
+            int pocet_hodu = 0;
+            Random kostka = new Random();
+
+            Console.WriteLine("Zadej mi kolik má kostka mít stěn");
+            while(!int.TryParse(Console.ReadLine(), out steny_kostky))
+            {
+                Console.WriteLine("Zadej číslo");
+            }
+
+            Console.WriteLine("Zadej mi kolikrát mám hodit kostkou");
+            while(!int.TryParse(Console.ReadLine(), out pocet_hodu))
+            {
+                Console.WriteLine("Zadej číslo");
+            }
+
+            double soucet_hodu = 0;
+            double pocet_hodu2 = 0;
+            
+            while(pocet_hodu2 < pocet_hodu)
+            {
+                soucet_hodu += kostka.Next(1, steny_kostky);
+                pocet_hodu2++;
+            }
+            Console.WriteLine($"Průměrný hod je {soucet_hodu/pocet_hodu2} z {pocet_hodu} hodů {steny_kostky} stěnnou kostkou!");
+        }
+    }
+}
---
+++ 0.3 while/zmrzlinar_oprava/zmrzlinar_oprava/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace zmrzlinar_oprava
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int vanilka = 15;
+            int coko = 20;
+            int zmrl = 0;
+
+            while (vanilka > 0 && coko > 0) 
+            {
+                Console.WriteLine($"Zadej jakou zmrzlinu chceš? Máme ještě {vanilka} vanilky a {coko} čokoládové zmrzliny (vanilka = 1, coko = 2, mix = 3)");
+                while(!int.TryParse(Console.ReadLine(), out zmrl))
+                {
+                    Console.WriteLine("Zadej jednu z možností");
+                }
+                if(zmrl == 1)
+                {
+                    vanilka--;
+                }else if(zmrl == 2)
+                {
+                    coko--;
+                } else if(zmrl == 3)
+                {
+                    vanilka--;
+                    coko--;
+                }
+            }
+            Console.WriteLine($"Zmrzlináři došli zásoby");
+            Console.WriteLine($"Zmrzlinář prodal {15 - vanilka} vanilky a {20 - coko} čokoládové zmrzliny");
+        }
+    }
+}
