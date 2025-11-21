af50ef69aebaf7396e6fd390ddb645b09bfa6a3c
HynekD1 <dominik.hynek343@gmail.com>
2025-11-21 12:52:37 +0100

    ok

---
+++ 0.4 for/oddelovaci_znak/oddelovaci_znak/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace oddelovaci_znak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string slovo = "Delta";
+            char oddelovac = '/';
+            string nove_slovo = "";
+
+            for (int i = 0; i < slovo.Length; i++)
+            {
+                if (!(i ==  slovo.Length -1))
+                {
+                    nove_slovo += slovo[i] + "" + oddelovac;
+                }
+                else
+                {
+                    nove_slovo += slovo[i];
+                }
+                
+            }
+            Console.WriteLine(nove_slovo);
+        }
+    }
+}
