61d9d14f96e154c6c55c1ec0e6f1c016a5999e05
HynekD1 <dominik.hynek343@gmail.com>
2025-11-21 12:38:20 +0100

    ok

---
+++ 0.4 for/Procviceni_obracene_slovo/Procviceni_obracene_slovo/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Procviceni_obracene_slovo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "pihovatý";
+            string obraceny_text = "";
+
+            //řádek níže je jediný, který budete měnit
+            for (int i = text.Length -1; i >= 0; i--)
+            {
+                obraceny_text += text[i];
+                
+            }
+            Console.WriteLine(obraceny_text);
+        }
+    }
+}
