0b9bb84a0d829ea3197fb7752f92a0aa1b927356
HynekD1 <dominik.hynek343@gmail.com>
2025-11-21 12:02:12 +0100

    ok

---
+++ 0.4 for/prumer_z_N/prumer_z_N/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Numerics;
+
+namespace prumer_z_N
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pocet_cisel = 0;
+            int soucet = 0;
+            Console.WriteLine("Kolik chceš vložit čísel(Budeš je muset zadat, tak si to rozmysli)");
+            while (!int.TryParse(Console.ReadLine(), out pocet_cisel))
+            {
+                Console.WriteLine("Zadej celé číslo!!");
+            }
+            for (int i = 0; i < pocet_cisel; i++)
+            {
+                int zadane_cislo = 0;
+                Console.WriteLine($"Zadej mi {i + 1}. číslo");
+                while (!int.TryParse(Console.ReadLine(), out zadane_cislo)) ;
+                {
+                    Console.WriteLine("Zadej mi celé číslo");
+                }
+                soucet += zadane_cislo;
+            }
+            Console.WriteLine($"Průměr ze zadaných čísel je {soucet/pocet_cisel}");
+
+            
+            
+
+            
+
+
+        }
+    }
+}
---
+++ 0.4 for/soucet_sudych/soucet_sudych/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Security;
+
+namespace soucet_sudych
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int soucet = 0;
+            for (int i = 1; i < 11; i++)
+            {
+                Console.WriteLine($"Zadej mi {i}. číslo");
+                int cislo = 0;
+                while (!int.TryParse(Console.ReadLine(), out cislo))
+                {
+                    Console.WriteLine("Zadej opravdu číslo!!!");
+                }
+                if (cislo % 2 == 0)
+                {
+                    soucet += cislo; 
+                }
+            }
+            Console.WriteLine($"součet sudých čísel je {soucet}");
+        }
+    }
+}
